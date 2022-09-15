422e61d3c84791865321148f9ea9c87c2045fa8e
Siydulislam <siydulislam333@gmail.com>
2022-09-15 07:13:48 +0600

    sign out user

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,5 @@
 import { Link, withRouter } from 'react-router-dom';
+import { isAuthenticated, signout } from '../utils/auth';
 
 const isActive = (history, path) => {
     if (history.location.pathname === path) {
@@ -15,14 +16,31 @@ const Menu = ({ history }) => {
                 <li className="nav-item">
                     <Link className="nav-link" style={isActive(history, '/')} to="/">Home</Link>
                 </li>
-                <li className="nav-item">
-                    <Link className="nav-link" style={isActive(history, '/login')} to="/login">Login</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" style={isActive(history, '/register')} to="/register">Register</Link>
-                </li>
+                {
+                    !isAuthenticated() &&
+                    <>
+                        <li className="nav-item">
+                            <Link className="nav-link" style={isActive(history, '/login')} to="/login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" style={isActive(history, '/register')} to="/register">Register</Link>
+                        </li>
+                    </>
+                }
+                {
+                    isAuthenticated() &&
+                    <li className="nav-item">
+                        <span
+                            className="nav-link"
+                            style={{ cursor: "pointer", color: "grey" }}
+                            onClick={() => signout(() => {
+                                history.push('/login')
+                            })}
+                        >Log Out</span>
+                    </li>
+                }
             </ul>
-        </nav>
+        </nav >
     )
 }
 
