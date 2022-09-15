1eef39813b310a2134fbd48ef2fe6e82892b32b8
Siydulislam <siydulislam333@gmail.com>
2022-09-15 08:20:28 +0600

    private route

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import { Switch, Route } from 'react-router-dom';
 import Home from './home/Home';
+import PrivateRoute from './protectedRoutes/PrivateRoute';
 import Dashboard from './user/Dashboard';
 import Login from './user/Login';
 import Register from './user/Register';
@@ -11,7 +12,9 @@ const Main = () => {
                 <Route path="/" exact component={Home} />
                 <Route path="/login" exact component={Login} />
                 <Route path="/register" exact component={Register} />
-                <Route path="/dashboard" exact component={Dashboard} />
+                <PrivateRoute path="/dashboard">
+                    <Dashboard />
+                </PrivateRoute>
             </Switch>
         </div>
     )
